3124830ac3cf4d7f29a600a122f0c5631f995c86
pc503n <margaretcpe@gmail.com>
2024-07-12 12:12:36 -0400

    Bring preview back

--- app/src/main/java/com/example/todolistapplication/home/HomeScreen.kt
+++ app/src/main/java/com/example/todolistapplication/home/HomeScreen.kt
@@ -14,10 +14,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.todolistapplication.TodoItem
 import com.example.todolistapplication.TodoItemDataModel
+import com.example.todolistapplication.db.TodoItemEntity
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -70,10 +73,67 @@ fun HomeScreen(
     }
 }
 
-// TODO: Bring this back
-//@Preview(showBackground = true)
-//@Composable
-//fun HomeScreenPreview() {
-//    val navController = rememberNavController()
-//    HomeScreen(navController)
-//}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenContent(
+    navController: NavHostController,
+    todoItems: List<TodoItemEntity>,
+    onUpdate: (TodoItemEntity) -> Unit,
+    onRemove: (TodoItemEntity) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "TODO LIST APP")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    Button(
+                        modifier = Modifier.padding(10.dp),
+                        onClick = {
+                            navController.navigate("todo_detail/new")
+                        }
+                    ) {
+                        Text("Add")
+                    }
+                }
+            )
+        }
+    ) { contentPadding ->
+        LazyColumn(modifier = Modifier.padding(contentPadding)) {
+            items(todoItems) { item ->
+                TodoItem(
+                    navController = navController,
+                    data = TodoItemDataModel(
+                        id = item.id.toString(),
+                        text = item.text,
+                        isDone = item.isDone
+                    ),
+                    onUpdate = {onUpdate(item)},
+                    onRemove = {onRemove(item)}
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    val navController = rememberNavController()
+    HomeScreenContent(
+        navController = navController,
+        todoItems = listOf(
+            TodoItemEntity(id = 1, text = "Sample Item 1"),
+            TodoItemEntity(id = 2, text = "Sample Item 2")
+        ),
+        onUpdate = {},
+        onRemove = {})
+}
